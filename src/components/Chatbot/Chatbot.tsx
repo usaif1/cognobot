8f88e7057f1113dd902e05b86988c40863531bac
usaif1@gmail.com <usaif1311@gmail.com>
2021-10-31 01:15:43 +0530

    added responsiveness

--- src/components/Chatbot/Chatbot.tsx
+++ src/components/Chatbot/Chatbot.tsx
@@ -8,9 +8,9 @@ import robo from "assets/robo.png";
 
 const Chatbot: React.FC = () => {
   return (
-    <div className="w-3/12 h-chatbot sm:w-11/12 mx-auto mt-10">
+    <div className="mx-auto mt-10 w-3/12 xl:w-5/12 2xl:w-4/12 h-chatbot lg:w-8/12 sm:w-11/12">
       <div
-        className="rounded-tr-2xl rounded-tl-2xl pl-14 pt-5 pb-12"
+        className="rounded-tr-2xl rounded-tl-2xl pl-14 pt-5 pb-10 sm:pl-8"
         style={{
           background:
             "linear-gradient(123.75deg, #F5821F 1.87%, #FF5C00 120.46%)",
@@ -26,7 +26,7 @@ const Chatbot: React.FC = () => {
         <p className="text-left text-lg text-white font-medium w-7/12">
           I am Iris, a Virtual Assistant How may I help you?
         </p>
-        <Spacer className="h-3" />
+        <Spacer className="h-4" />
         <RoundedIcon src={robo} size={16} iconSize={8} bgColor="orange2" />
       </div>
       <div className="h-2/3 border border-black rounded-br-2xl rounded-bl-2xl" />
